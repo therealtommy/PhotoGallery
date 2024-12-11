92c25ed4651360f2dc622fe97cecd7433471ec08
tommyallen <thomasclansi228@gmail.com>
2024-12-12 00:11:19 +0700

    Create variable for thumbnailDownloader

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,8 +1,10 @@
 package com.example.photogallery
 
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -33,17 +35,24 @@ class PhotoGalleryFragment : Fragment() {
             RecyclerView
     private lateinit var photoGalleryViewModel:
             PhotoGalleryViewModel
+    private lateinit var thumbnailDownloader : ThumbnailDownloader<PhotoHolder>
 
 
     override fun onCreate(savedInstanceState:
                           Bundle?) {
         super.onCreate(savedInstanceState)
+        retainInstance = true
+        setHasOptionsMenu(true)
 
-        photoGalleryViewModel =
-            ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
+        val responseHandler = Handler()
 
-
-
+        thumbnailDownloader =
+            ThumbnailDownloader(responseHandler) { photoHolder, bitmap ->
+                val drawable = BitmapDrawable(resources, bitmap)
+                photoHolder.bindDrawable(drawable)
+            }
+        photoGalleryViewModel = ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
+        lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
     }
 
 
